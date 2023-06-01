9b9a7b71f56dfaae4205e5922f2c3dc620bcd8e9
Elkana Muthengi <elkyem@gmail.com>
2023-06-01 16:32:54 +0300

    Added code to java file

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+public class IfElse{
+	
+	public static void main(String[] args){
+		
+		boolean gameOver = true;
+		int score = 800;
+		int levelCompleted = 5;
+		int bonus = 100;
+		
+		int finalScore = score;
+		
+		if(gameOver){
+			finalScore += (levelCompleted * bonus);
+			System.out.println("Your final score was " + finalScore);
+		}
+		
+	}
+	
+}
